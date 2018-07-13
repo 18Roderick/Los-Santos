7ac198bb1dafe256bbd843ba6a62807fc7265426
18Roderick <rjrr507@gmail.com>
2018-07-12 23:55:03 -0500

    initial commit con estructura del proyecto

---
+++ header.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="./public/css/index.css">
+<link rel="stylesheet" href="./public/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="./public/js/index.js"></script>
+
+
+
+<div class="container-banner">
+<div class="topnav" id="myTopnav">
+    <a href="./" class="inicio">A lo Santeño</a>
+    <a href="./historia.php">Historia</a>
+    <a href="./gastronomia.php">Gastronomía</a>
+
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
+</div>
+
+  <div class="titulo">
+     <h1 >Los Santos</h1>
+  </div>
+ 
+</div>
